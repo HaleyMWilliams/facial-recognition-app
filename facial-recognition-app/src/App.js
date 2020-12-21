4ab2b5194549a8604ea43f1e8c91a3feec71351c
HaleyMWilliams <66376569+HaleyMWilliams@users.noreply.github.com>
2020-12-21 18:29:05 -0500

    clarifai-API
    added clarifai face detection API and face reognition component. Currently only loads the image and console logs the response.

--- facial-recognition-app/src/App.js
+++ facial-recognition-app/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from "./components/Navigation/Navigation";
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import { Component } from 'react';
+
+const app = new Clarifai.App({
+ apiKey: '03ecc1108a3442959b1c045faf44c9a8'
+});
 
 const particlesOptions = {
   particles: {
@@ -23,34 +29,44 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log('click');
-  }
-
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+        .then(
+          function(response) {
+            console.log(response.outputs[0].data.regions[0].region_info.bounding_box); },
+            function(err) {
+              console.log(err)
+            }
+        );
+       }
 
   render() {
     return (
     <div className="App">
-    <Particles 
-    className= 'particles'
-    params={particlesOptions}
-    />
-    <Navigation />
-    <Logo />
-    <Rank />
-    <ImageLinkForm 
-    onInputChange={this.onInputChange} 
-    onButtonSubmit={this.onButtonSubmit}/>
-    {/* 
-<FaceRecognition /> */}
-         </div>
+      <Particles 
+        className= 'particles'
+        params={particlesOptions}
+      />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}/>
+      <FaceRecognition imageUrl={this.state.imageUrl}/>
+    </div>
   );
   }
 }
