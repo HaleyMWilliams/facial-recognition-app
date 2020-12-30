1ba8a597c6f52bc3cc194010b6443110e8156cb6
HaleyMWilliams <66376569+HaleyMWilliams@users.noreply.github.com>
2020-12-30 15:34:28 -0500

    updated-register
    updated the Register component to create a new user

--- facial-recognition-app/src/components/Register/Register.js
+++ facial-recognition-app/src/components/Register/Register.js
@@ -22,6 +22,25 @@ onPasswordChange = (event) => {
   this.setState({password: event.target.value})
 }
 
+onSubmitSignIn = () => {
+  fetch('http://localhost:3005/signin', {
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      email: this.state.email,
+      password: this.state.password,
+      name: this.state.name
+    })
+  })
+  .then(response => response.json())
+  .then(user => {
+    if (user) {
+      this.props.loadUser(user)
+      this.props.onRouteChange('home');
+    }
+  })
+}
+
   render() {
     return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
@@ -36,6 +55,7 @@ onPasswordChange = (event) => {
                     type="text"
                     name="name"
                     id="name"
+                    onChange={this.onNameChange}
                 />
               </div>
               <div className="mt3">
@@ -45,6 +65,7 @@ onPasswordChange = (event) => {
                     type="email"
                     name="email-address"
                     id="email-address"
+                    onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
@@ -54,12 +75,13 @@ onPasswordChange = (event) => {
                     type="password"
                     name="password"
                     id="password"
+                    onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
             <div className="">
               <input
-                onClick={() => onRouteChange('home')}
+                onClick={this.onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Register"
