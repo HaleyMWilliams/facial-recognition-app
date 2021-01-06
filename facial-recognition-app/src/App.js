a4aef4426867ff593ce34108fd6585b8036cacdd
HaleyMWilliams <66376569+HaleyMWilliams@users.noreply.github.com>
2021-01-06 14:10:37 -0500

    fixed-user-bug
    fixed bug that showed the previously logged in user's info by setting the user object's state to have empty string values at sign out

--- facial-recognition-app/src/App.js
+++ facial-recognition-app/src/App.js
@@ -26,10 +26,7 @@ const particlesOptions = {
   }
 }
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
+const initialState = {
       input: '',
       imageUrl: '', 
       box: {},
@@ -43,6 +40,11 @@ class App extends Component {
             joined: ''
       }
     }
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = initialState;
   }
 
 loadUser = (data) => {
@@ -96,6 +98,7 @@ this.setState({box: box});
             .then(count => {
               this.setState(Object.assign(this.state.user, {entries: count}))
             })
+            .catch(console.log)
 
           }
         this.displayFaceBox(this.calculateFaceLocation(response))
@@ -105,7 +108,7 @@ this.setState({box: box});
 
 onRouteChange = (route) => {
   if (route === 'signout') {
-      this.setState({isSignedIn: false })
+      this.setState(initialState)
   } else if (route === 'home') {
       this.setState({isSignedIn: true })
   }
